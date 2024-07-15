ae3f264a3878ff18443afa8b8f006ddf97c9c995
Baekrok <152347509+baekrok@users.noreply.github.com>
2024-07-15 16:59:32 +0900

    Add files via upload

---
+++ tiny-imagenet_preprocess.py
@@ -0,0 +1,49 @@
+# Modified from https://github.com/kennethleungty/PyTorch-Ignite-Tiny-ImageNet-Classification.git
+import glob
+import os
+import shutil
+import pandas as pd
+
+DATA_DIR = './data/tiny-imagenet-200'
+TRAIN_DIR = "./data/tiny-imagenet-200/train/"
+VALID_DIR = "./data/tiny-imagenet-200/val/"
+
+val_data = pd.read_csv(f'{VALID_DIR}/val_annotations.txt', 
+                       sep='\t', 
+                       header=None, 
+                       names=['File', 'Class', 'X', 'Y', 'H', 'W'])
+
+# Create separate validation subfolders for the validation images based on
+# their labels indicated in the val_annotations txt file
+val_img_dir = os.path.join(VALID_DIR, 'images')
+
+# Open and read val annotations text file
+fp = open(os.path.join(VALID_DIR, 'val_annotations.txt'), 'r')
+data = fp.readlines()
+
+# Create dictionary to store img filename (word 0) and corresponding
+# label (word 1) for every line in the txt file (as key value pair)
+val_img_dict = {}
+for line in data:
+    words = line.split('\t')
+    val_img_dict[words[0]] = words[1]
+fp.close()
+
+
+# Create subfolders (if not present) for validation images based on label ,
+# and move images into the respective folders
+for img, folder in val_img_dict.items():
+    newpath = (os.path.join(val_img_dir, folder))
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    if os.path.exists(os.path.join(val_img_dir, img)):
+        os.rename(os.path.join(val_img_dir, img), os.path.join(newpath, img))
+        
+# Save class names (for corresponding labels) as dict from words.txt file
+class_to_name_dict = dict()
+fp = open(os.path.join(DATA_DIR, 'words.txt'), 'r')
+data = fp.readlines()
+for line in data:
+    words = line.strip('\n').split('\t')
+    class_to_name_dict[words[0]] = words[1].split(',')[0]
+fp.close()
